b6f3ec330dee2bb75052ff15eca535eec87925e7
Bethany Fannin <contact@bethanyfannin.com>
2026-01-27 17:56:33 -0500

    build: add changelogen config file

---
+++ changelog.config.js
@@ -0,0 +1,24 @@
+export default {
+  types: {
+    feat: { title: '🚀 New Features', semver: 'minor' },
+    fix: { title: '🩹 Bug Fixes', semver: 'patch' },
+    docs: { title: '📖 Documentation' },
+    style: { title: '🎨 Code Style' },
+    refactor: { title: '💅 Refactors' },
+    perf: { title: '🔥 Performance Improvements' },
+    test: { title: '✅ Tests' },
+    build: { title: '📦 Build' },
+    ci: { title: '🤖 CI' },
+    chore: { title: '🏡 Chore' },
+    revert: { title: '⏪ Revert' }
+  },
+  scopeMap: {},
+  tokens: {
+    github: process.env.CHANGELOGEN_TOKENS_GITHUB || process.env.GITHUB_TOKEN || process.env.GH_TOKEN
+  },
+  templates: {
+    commitMessage: 'chore(release): v{{newVersion}}',
+    tagMessage: 'v{{newVersion}}',
+    tagBody: 'v{{newVersion}}'
+  }
+}
