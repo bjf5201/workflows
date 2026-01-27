5e347bac55ea2a7c42dc7b1f0695b8628bc2967b
Bethany Fannin <contact@bethanyfannin.com>
2026-01-27 14:52:20 -0500

    chore: add personalized rules to commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,12 @@
 const Configuration = {
     extends: ["@commitlint/config-conventional"],
+    rules: {
+        'header-max-length': [2, 'always', 100],
+        'body-leading-blank': [2, 'always'],
+        'footer-leading-blank': [2, 'always'],
+        'body-empty': [2, 'never'],
+        'subject-min-length': [2, 'always', 30]
+    }
 }
 
 export default Configuration;
